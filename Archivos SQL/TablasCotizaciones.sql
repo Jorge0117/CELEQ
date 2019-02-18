d3a00c126f09a911c8a3dd2f690266aac45ae975
Estiven <estivenalg@gmail.com>
2019-02-18 09:08:29 -0600

    Archivos sql

---
+++ Archivos SQL/TablasCotizaciones.sql
@@ -0,0 +1,81 @@
+CREATE TABLE Cliente
+(
+	nombre		VARCHAR(255)	PRIMARY KEY NOT NULL,
+	telefono	VARCHAR(255),
+	telefono2	VARCHAR(255),
+	correo		VARCHAR(255),
+	fax			VARCHAR(255),
+	direccion	VARCHAR(255),
+	atencionDe	VARCHAR(255)						
+)
+
+CREATE TABLE Cotizacion
+(
+	consecutivo		VARCHAR(100)	PRIMARY KEY NOT NULL,
+	licitacion		BIT,
+	observaciones	VARCHAR(255),
+	precioMuestreo	FLOAT,
+	descuento		FLOAT,
+	gastosAdm		FLOAT,
+	fechaLimite		DATE,
+	fechaSolicitud	DATE,
+	fechaRespuesta	DATE,
+	saldoAfavor		FLOAT,
+	granTotal		FLOAT,
+	moneda			CHAR,
+	cotizador		NVARCHAR(50),
+	cliente			VARCHAR(255)
+
+	FOREIGN KEY(cotizador)	REFERENCES Usuarios(nombreUsuario),
+	FOREIGN KEY(cliente)	REFERENCES Cliente(nombre)
+)
+
+CREATE TABLE Gira
+(
+	id						INT		IDENTITY(1,1)	PRIMARY KEY,
+	horasMuestreo			INT,
+	cantidadProfesionales	INT,
+	nochesAlojamiento		INT,
+	cantidadTecnicos		INT,
+	gastoTotal				FLOAT,
+	provincia				VARCHAR(100),
+	localidad				VARCHAR(100),
+	idCotizacion			VARCHAR(100)
+
+	FOREIGN KEY(idCotizacion)	REFERENCES Cotizacion(consecutivo)	
+)
+
+CREATE TABLE tipoAnalisis
+(
+	tipo	VARCHAR(100)	PRIMARY KEY NOT NULL
+)
+
+CREATE TABLE Muestra
+(
+	id					INT		IDENTITY(1,1)	PRIMARY KEY,
+	especifique			VARCHAR(100),
+	empaque				BIT,
+	sellada				BIT,
+	numeroMuestras		INT,
+	cantidadNecesaria	VARCHAR(50),
+	tipoAnalisis		VARCHAR(100)
+
+	FOREIGN KEY(tipoAnalisis) REFERENCES tipoAnalisis(tipo)
+)
+
+CREATE TABLE Analisis
+(
+	descripcion			VARCHAR(255)	PRIMARY KEY NOT NULL,
+	metodo				VARCHAR(255),
+	precio				INT,
+	acreditacion		TINYINT,
+	tipoAnalisis		VARCHAR(100)
+
+	FOREIGN KEY(tipoAnalisis) REFERENCES tipoAnalisis(tipo)
+)
+
+CREATE TABLE Localizaciones
+(
+	provincia		VARCHAR(100),
+	canton			VARCHAR(100)
+)
